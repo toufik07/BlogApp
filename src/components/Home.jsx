c8261b91adfa22cd3c5acf22adc007ec4b040bbf
Toufik Badkar <toufikbadkar80@gmail.com>
2023-12-30 17:01:15 +0530

    added responsive design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
 
 
     const [blogs, setBlogs] = useState(null)
-    const [load,setLoad] = useState(true)
+    const [load, setLoad] = useState(true)
 
     function loadData() {
         // setLoad(true)
@@ -22,14 +22,14 @@ export default function Home() {
 
 
 
-    function handleDelete(e,id) {
+    function handleDelete(e, id) {
         // const data = blogs.filter((blogs) => blogs.id !== id);
-         e.preventDefault();
-        axios.delete("https://6588549890fa4d3dabf9c2e2.mockapi.io/blogs/"+id)
-        .then((res) => {
-            console.log(res.data);
-            loadData();
-        })
+        e.preventDefault();
+        axios.delete("https://6588549890fa4d3dabf9c2e2.mockapi.io/blogs/" + id)
+            .then((res) => {
+                console.log(res.data);
+                loadData();
+            })
         // setBlogs();
     }
 
@@ -40,36 +40,56 @@ export default function Home() {
     // if difine dependancies as [name] then whenever the name changes the useeffect will be call
     // in every it call once when the component render
     useEffect(() => {
-       loadData();
-    },[])
+        console.log("data");
+        loadData();
+    }, [])
 
     return (
         <>
-        { load && <p style={{
-                    textAlign :"center"
-                 }}>Loading-Data....</p> }
-        {blogs &&
-            <div className="content">
-                 {
-                    blogs.map((blog) => (
-                        <div className="home" key={blog.id}>
-                            <div className="data" >
-                                <div className="part1" >
-                                    <h2>author : {blog.author}</h2>
-                                    <p>Data : {blog.body}</p>
-                                </div>
-                                <div className="part2">
-                                    <Link to={'/create/'+ blog.id}> <button id='btn1'>Edit</button> </Link>
-                                    <button id='btn2' onClick={(e) => {
-                                        handleDelete(e,blog.id)
-                                    }}>Delete</button>
+            {load && <p style={{
+                textAlign: "center"
+            }}>Loading-Data....</p>}
+            {blogs &&
+                <div className="content">
+                    {
+                        blogs.map((blog) => (
+                            // <div className="home" key={blog.id}>
+                            //     <div className="data" >
+                            //         <div className="part1" >
+                            //             <h2>author : {blog.author}</h2>
+                            //             <p>Data : {blog.body}</p>
+                            //         </div>
+                            //         <div className="part2">
+                            //             <Link to={'/create/'+ blog.id}> <button id='btn1'>Edit</button> </Link>
+                            //             <button id='btn2' onClick={(e) => {
+                            //                 handleDelete(e,blog.id)
+                            //             }}>Delete</button>
+                            //         </div>
+                            //     </div>
+                            // </div>
+                            <div className="container" key={blog.id} style={{
+                                margin:"2%",
+                                padding:"1%"
+                            }}>
+                                <div class="card" >
+                                    <div class="card-header  bg-success-subtle text-emphasis-success" >
+                                        {blog.author}
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{blog.title}</h5>
+                                        <p class="card-text">{blog.body}</p>
+                                        <Link to={'/create/' + blog.id}> <button className='btn btn-primary'>Edit</button> </Link> 
+                                        <button className="btn btn-danger" onClick={(e) => {
+                                            handleDelete(e, blog.id)
+                                        }}>Delete</button> 
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    ))
-                }
-            </div>  
-        }
+
+                        ))
+                    }
+                </div>
+            }
         </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,14 +3,36 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
     return (
-        <div className="navbar">
-            <div className="nav1">
-                <h1>Blog Website</h1>
+        // <div className="navbar navbar-expand-lg navbar-light bg-light" >
+        //     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        //         <span class="navbar-toggler-icon"></span>
+        //     </button>
+        //     <div className=" collapse navbar-collapse" id="navbarTogglerDemo01">
+        //         <h2>BLOG</h2>
+        //     </div>
+        //     <ul className=' navbar-nav mr-auto mt-2 mt-lg-0'>
+        //         <li class="nav-item active"><Link to={'/'} className='link'>Home</Link></li>
+        //         <li class="nav-item"><Link to={'/create'} className='link'>New-Blog</Link></li>
+        //     </ul>
+        // </div >
+
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <h2 className='navbar-brand '>BLOG</h2>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse text-center" id="navbarNavAltMarkup">
+                    <ul class="nav justify-content-center text-center">
+                        <li class="nav-item">
+                        <Link to={'/'} className='nav-link'>Home</Link>
+                        </li>
+                        <li class="nav-item">
+                        <Link to={'/create'} className='nav-link'>New-Blog</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <ul className='nav2'>
-                <li><Link to={'/'} className='link'>Home</Link></li>
-                <li><Link to={'/create'} className='link'>New-Blog</Link></li>
-            </ul>
-        </div>
+        </nav>
     )
 }
